2f89f7b7e58a4116b0429aab84b1854614bc52eb
goldwinramirez07 <goldwinramirez07@gmail.com>
2025-11-11 22:58:30 +0800

    Middleware and Log in UI
    added middleware file
    minor changes in views

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,27 @@
 
 
 @section('content')
-<div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-    <header>
-        <div>
-            <h1 class="title"><i>UrbanSafe</i></h1>
+    <center>
+        <div
+            class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div
+                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        <a href="/login">UrbanSafe</a>
+                    </h1>
+                    <div>
+                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base px-9 py-4 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                            <a href="#">Report Incident</a></button>
+                    </div>
+                    <div>
+                        <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                            <a href="/login">Login</a></button>
+                    </div>
+                    <div>
+                        <label class="text-sm">New here? Try us now! <a href="/register">Sign up</a></label>
+                    </div>
+                </div>
+            </div>
         </div>
-    </header>
-    <form action="#" method="post">
-        <input type="submit" class="report-btn" value="MAKE A REPORT">
-        <br><br> 
-    </form>
-    <form action="#" method="post">
-        <input type="submit" class="login-btn" value="LOGIN">
-    </form>
-</div>
+    </center>
