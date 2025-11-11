123c0c0ac07ac669b20576cdecaed7763f7ba1ec
goldwinramirez07 <goldwinramirez07@gmail.com>
2025-11-11 20:24:11 +0800

    Landing Pages
    Created sample input forms for the ff:
    Landing page
    Login
    Register

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+    <header>
+        <div>
+            <h1 class="title"><i>UrbanSafe</i></h1>
+        </div>
+    </header>
+    <form action="#" method="post">
+        <input type="submit" class="report-btn" value="MAKE A REPORT">
+        <br><br> 
+    </form>
+    <form action="#" method="post">
+        <input type="submit" class="login-btn" value="LOGIN">
+    </form>
+</div>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+    <form>
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Input Name" required="">
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="youremail@domain.com" required="">
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="********" required="">
+        </div>
+    </form>
+</div>
