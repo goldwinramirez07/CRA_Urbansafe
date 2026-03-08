956829f0b4a48c5311b481ba6a045e27a7f02e31
Goldwin Ramirez <goldwinramirez07@gmail.com>
2026-03-09 00:03:14 +0800

    Katalon Test: Views/Index.blade.php
    Test Goal:
    Verify the following Elements/Objects:
    link_urbansafe
    link_signup
    btn_login
    btn_report_incident
    
    7 Steps automated test - Refer to Test Cases/Views/TC_Index_Page_Render
    
    Result (7/7)Steps (1/1)Test Case:
    1 - openBrowser("")
    2.538s
    2 - navigateToUrl("http://localhost:8000/")
    4.846s
    3 - verifyElementPresent(findTestObject("Pages/Index/link_urbansafe"), 10)
    461ms
    4 - verifyElementPresent(findTestObject("Pages/Index/btn_report_incident"), 10)
    239ms
    5 - verifyElementPresent(findTestObject("Pages/Index/btn_login"), 10)
    248ms
    6 - verifyElementPresent(findTestObject("Pages/Index/link_signup"), 10)
    263ms
    7 - closeBrowser()
    148ms

---
+++ Test Suites/TS_ViewTests.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_ViewTests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3c96cd34-1056-48b8-b2ce-1b48b2e822fe</testSuiteGuid>
+</TestSuiteEntity>
